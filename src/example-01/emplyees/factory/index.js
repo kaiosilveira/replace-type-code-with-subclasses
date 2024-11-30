1e74c99d0f447b7e04a0d0df9cc8aca267468eea
Kaio Silveira <silveira.kaio@icloud.com>
2024-11-30 15:09:56 +0000

    ex-1: return `Salesman` subclass when `type` is `salesman` at `createEmployee`

--- src/example-01/emplyees/factory/index.js
+++ src/example-01/emplyees/factory/index.js
@@ -1,10 +1,13 @@
 import { Employee } from '..';
 import { Engineer } from '../engineer';
+import { Salesman } from '../salesman';
 
 export const createEmployee = (name, type) => {
   switch (type) {
     case 'engineer':
       return new Engineer(name);
+    case 'salesman':
+      return new Salesman(name);
   }
   return new Employee(name, type);
 };
--- src/example-01/emplyees/factory/index.test.js
+++ src/example-01/emplyees/factory/index.test.js
@@ -1,5 +1,6 @@
 import { createEmployee } from '.';
 import { Engineer } from '../engineer';
+import { Salesman } from '../salesman';
 
 describe('createEmployee', () => {
   it('should create an engineer', () => {
@@ -7,5 +8,11 @@ describe('createEmployee', () => {
     expect(employee.type).toBe('engineer');
     expect(employee.toString()).toBe('Kaio (engineer)');
     expect(employee instanceof Engineer).toBe(true);
+  });
+
+  it('should create a salesman', () => {
+    const employee = createEmployee('Kaio', 'salesman');
+    expect(employee.type).toBe('salesman');
+    expect(employee instanceof Salesman).toBe(true);
   });
 });
