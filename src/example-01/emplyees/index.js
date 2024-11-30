f2cbc5e6c79a633310a8867adb1ae6ad5e78c5b8
Kaio Silveira <silveira.kaio@icloud.com>
2024-11-30 15:19:18 +0000

    ex-1: remove type validation at base `Employee` superclass

--- src/example-01/emplyees/index.js
+++ src/example-01/emplyees/index.js
@@ -1,12 +1,6 @@
 export class Employee {
   constructor(name, type) {
-    this.validateType(type);
     this._name = name;
-  }
-
-  validateType(arg) {
-    if (![`engineer`, `manager`, `salesman`].includes(arg))
-      throw new Error(`Employee cannot be of type ${arg}`);
   }
 
   toString() {
--- src/example-01/emplyees/index.test.js
+++ src/example-01/emplyees/index.test.js
@@ -1,10 +1,6 @@
 import { Employee } from '.';
 
 describe('Employee', () => {
-  it('should throw an error if type is not valid', () => {
-    expect(() => new Employee('Kaio', 'invalid')).toThrow('Employee cannot be of type invalid');
-  });
-
   describe('toString', () => {
     it('should return the name and type', () => {
       class DummyEmployee extends Employee {
